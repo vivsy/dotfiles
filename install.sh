8a5544a5ee358952119338773ae93fb272614cb4
Vivian Huang <vhuang13@gmail.com>
2022-03-17 15:40:58 -0700

    Add git config

--- install.sh
+++ install.sh
@@ -1,3 +1,21 @@
 #!/bin/sh
+
 set -eu
-ln -sfv ~/.dotfiles/zshrc.sh ~/.zshrc
+
+printf 'warning: this may delete files. ok? [y/n] '
+read -r ans
+if [ "$ans" != y ]; then
+  echo "answer was not 'y'"
+  exit 1
+fi
+
+rm -rf ~/.hushlogin
+touch ~/.hushlogin
+chmod 600 ~/.hushlogin
+
+rm -rf ~/.zshrc
+ln -s ~/.dotfiles/zshrc.sh ~/.zshrc
+
+mkdir -p ~/.config
+rm -rf ~/.config/git
+ln -sf ~/.dotfiles/git ~/.config/git
