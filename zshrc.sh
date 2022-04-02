8ef53380db9f58ff420a5b8969853544355673b0
Vivian Huang <vhuang13@gmail.com>
2022-04-02 11:34:59 -0700

    Use double quotes

--- zshrc.sh
+++ zshrc.sh
@@ -1,7 +1,6 @@
-export EDITOR='code'
-export VISUAL='vim'
-export PAGER='less'
-export LESS='-g -i -M -R -S -w -z-4'
+export EDITOR="code"
+export PAGER="less"
+export LESS="-g -i -M -R -S -w -z-4"
 
 setopt autocd
 setopt ignoreeof
@@ -21,40 +20,40 @@ setopt appendhistory
 setopt sharehistory
 setopt incappendhistory
 
-alias -- '-'='cd -'
-alias ..='cd ..'
+alias -- "-"="cd -"
+alias ..="cd .."
 alias e="$EDITOR"
-alias grep='grep --color=auto'
-alias ls='exa --group-directories-first'
+alias grep="grep --color=auto"
+alias ls="exa --group-directories-first"
 
-alias ga='git add'
-alias gb='git branch'
-alias gc='git commit'
-alias gd='git diff'
-alias gh='git show --format=fuller'
-alias gk='git checkout'
-alias gl='git log --graph --oneline'
-alias gp='git push'
-alias gr='git reset'
-alias gs='git status'
+alias ga="git add"
+alias gb="git branch"
+alias gc="git commit"
+alias gd="git diff"
+alias gh="git show --format=fuller"
+alias gk="git checkout"
+alias gl="git log --graph --oneline"
+alias gp="git push"
+alias gr="git reset"
+alias gs="git status"
 
 autoload -U compinit
 compinit -i
-zstyle ':completion:*' group-name ''
-zstyle ':completion:*' list-dirs-first true
-zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}'
-zstyle ':completion:*' menu select
-zstyle ':completion:*' squeeze-slashes true
-zstyle ':completion:*' verbose false
+zstyle ":completion:*" group-name ""
+zstyle ":completion:*" list-dirs-first true
+zstyle ":completion:*" matcher-list "m:{a-zA-Z}={A-Za-z}"
+zstyle ":completion:*" menu select
+zstyle ":completion:*" squeeze-slashes true
+zstyle ":completion:*" verbose false
 
-bindkey '^r' history-incremental-search-backward
-bindkey '^s' history-incremental-search-forward
+bindkey "^r" history-incremental-search-backward
+bindkey "^s" history-incremental-search-forward
 
 . ~/.zplug/init.zsh
 
-zplug 'mafredri/zsh-async', from:github
-zplug 'sindresorhus/pure', use:pure.zsh, from:github, as:theme
-zplug 'zdharma/fast-syntax-highlighting', from:github
+zplug "mafredri/zsh-async", from:github
+zplug "sindresorhus/pure", use:pure.zsh, from:github, as:theme
+zplug "zdharma/fast-syntax-highlighting", from:github
 
 if ! zplug check; then
   zplug install
